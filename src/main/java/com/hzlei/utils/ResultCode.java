cb6e4bb0ddfd5c1da88e3d6be8918f4aaea0ddf9
hzlei <hzlei@foxmail.com>
2021-04-07 15:39:58 +0800

    统一返回结果

---
+++ src/main/java/com/hzlei/utils/ResultCode.java
@@ -0,0 +1,11 @@
+package com.hzlei.utils;
+
+/**
+ * @author hzlei
+ * @date 2021/04/07 11:20
+ * Description  状态码
+ */
+public interface ResultCode {
+    Integer SUCCESS = 200; //成功
+    Integer ERROR = 500; //失败
+}
